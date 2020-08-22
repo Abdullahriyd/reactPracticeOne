5c98289d4d32f0416643dc230163a98f0e995695
Abdullahriyd <Abdullahriyd>
2020-08-23 00:28:08 +0600

    react practice

--- src/App.js
+++ src/App.js
@@ -23,13 +23,14 @@ function Person(e) {
 
 function CountNow(){
   const [count, setCount] = useState(0);
-  const [countNeg, setCountNeg] = useState(0);
+  const design = {margin: '10px', padding: '5px', backgroundColor: "green", color: 'white'}
   return(
     <div>
-      <button onClick={()=> setCount(count+1) }>Click Me Count Plus {count}</button>
-      <button onClick={()=> setCountNeg(countNeg-1) }>Click Me Count Minus {countNeg}</button>
-    </div>
-    
+      <h2>Age: {count}</h2>
+      <button style={design} onClick={()=> setCount(count+1) }>Click Me Count Plus {count}</button>
+      <button style={{backgroundColor: 'orange',margin: '10px', padding: '5px',color:'black'}} onClick={()=> count>0 ? setCount(count-1) : setCount(0)}>Click Me Count Minus {count}</button><br/>
+      <button style={{backgroundColor: 'red',margin: '10px', padding: '5px',color:'white'}} onClick={()=> {setCount(0)} }>Reseat Button</button>
+    </div>  
   )
 }
 
